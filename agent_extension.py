5f28aa4b6d069dd255a1b13879c25229f9931ae4
Marcelo Mergulhao <marcelo.mergulhao@phiinnovations.com>
2017-08-07 17:23:29 -0300

    Adicionando versão com query do DB e escrita em objetos mapeados pelo JSON

--- agent_extension.py
+++ agent_extension.py
@@ -1,14 +1,114 @@
 #!/usr/bin/python -u
+from sqlalchemy.orm import sessionmaker
+from db_engine import engine, Usuario, Modulo, AlarmeConfig, RedeSeguranca, DataLog
+from db_engine import TimeServer, EmailServer, AlarmLog, Parameters
+import json
+import sys
+import logging
+
 import snmp_passpersist as snmp
 
-element_list = {"0": "batStatus",
-                "1": "nBatBank"}
-value_list = {"batStatus": 1,
-              "nBatBank": 2}
+# logging.basicConfig(filename='/home/prjs/cm_comandos_lineares/equalizer-agent/log.log',level=logging.DEBUG,
+logging.basicConfig(filename='/home/root/equalizer-agent/log.log',level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M')
+logging.info("Iniciando agente extra")
+
+# Create DB sessionmaker, every request will have open and close a new session
+Session = sessionmaker(bind=engine)
+
+# Load json
+# with open("/home/prjs/cm_comandos_lineares/equalizer-agent/element_mib.json", "r") as fd:
+with open("/home/root/equalizer-agent/element_mib.json", "r") as fd:
+    element_dic = json.loads(fd.read())
+    # Set up inverted lookup
+    element_dic_inv = {}
+    for e in element_dic:
+        element_dic_inv[element_dic[e]] = e
+
+logging.info("Carregado arquivo de elmentos/OID")
+
+def addParametersToOIDs(parameter, pp):
+    logging.info("Adiciona parameter nos OIDs")
+    pp.add_str(element_dic_inv["avg_max"], parameter.avg_last)
+    pp.add_str(element_dic_inv["duty_min"], parameter.duty_min)
+    pp.add_str(element_dic_inv["duty_max"], parameter.duty_max)
+    pp.add_str(element_dic_inv["cte_index"], parameter.cte_index)
+    pp.add_str(element_dic_inv["delay"], parameter.delay)
+    pp.add_str(element_dic_inv["num_cycles_var_read"], parameter.num_cycles_var_read)
+    pp.add_str(element_dic_inv["bus_voltage"], parameter.bus_voltage)
+    pp.add_str(element_dic_inv["save_log_time"], parameter.save_log_time)
+    pp.add_str(element_dic_inv["disk_capacity"], parameter.disk_capacity)
+    pp.add_str(element_dic_inv["param1"], parameter.param1)
+    pp.add_str(element_dic_inv["param2"], parameter.param2)
+    pp.add_str(element_dic_inv["param3"], parameter.param3)
+    pp.add_str(element_dic_inv["param4"], parameter.param4)
+    pp.add_str(element_dic_inv["param5"], parameter.param5)
+    pp.add_str(element_dic_inv["param6"], parameter.param6)
+    pp.add_str(element_dic_inv["param7"], parameter.param7)
+    pp.add_str(element_dic_inv["param8"], parameter.param8)
+    pp.add_str(element_dic_inv["param9"], parameter.param9)
+    pp.add_str(element_dic_inv["param10"], parameter.param10)
+
+def addModultoToOIDs(modulo, pp):
+    logging.info("Adiciona modulo nos OIDs")
+    pp.add_str(element_dic_inv["descricao"], modulo.descricao)
+    pp.add_str(element_dic_inv["tensao_nominal"], str(modulo.tensao_nominal))
+    pp.add_int(element_dic_inv["capacidade_nominal"], modulo.capacidade_nominal)
+    pp.add_int(element_dic_inv["n_strings"], modulo.n_strings)
+    pp.add_int(element_dic_inv["n_baterias_por_strings"], modulo.n_baterias_por_strings)
+    pp.add_str(element_dic_inv["contato"], modulo.contato)
+    pp.add_str(element_dic_inv["localizacao"], modulo.localizacao)
+    pp.add_str(element_dic_inv["fabricante"], modulo.fabricante)
+    pp.add_str(element_dic_inv["tipo"], modulo.tipo)
+    pp.add_str(element_dic_inv["data_instalacao"], modulo.data_instalacao)
+    pp.add_int(element_dic_inv["conf_alarme_id"], modulo.conf_alarme_id)
+
+def addUsuarioToOIDs(user, pp):
+    logging.info("Adiciona usuario nos OIDs")
+    pp.add_str(element_dic_inv["nome"], user.nome)
+    pp.add_str(element_dic_inv["sobreNome"], user.sobreNome)
+    pp.add_str(element_dic_inv["telefone"], user.telefone)
+    pp.add_str(element_dic_inv["email"], user.email)
+    pp.add_str(element_dic_inv["senha"], user.senha)
+    pp.add_str(element_dic_inv["acesso"], user.acesso)
+
+def addRedeToOIDs(rede, pp):
+    logging.info("Adiciona rede nos OIDs")
+    pp.add_str(element_dic_inv["mac"], rede.mac)
+    pp.add_str(element_dic_inv["velocidadePlacaRede"], rede.velocidadePlacaRede)
+    pp.add_str(element_dic_inv["localAddress"], rede.localAddress)
+    pp.add_str(element_dic_inv["gateway"], rede.gateway)
+    pp.add_str(element_dic_inv["mascara"], rede.mascara)
+    pp.add_str(element_dic_inv["servidorDNS"], rede.servidorDNS)
+    pp.add_str(element_dic_inv["nomeDoSistema"], rede.nomeDoSistema)
+    pp.add_str(element_dic_inv["localDoSistema"], rede.localDoSistema)
+    pp.add_str(element_dic_inv["contatoDoSistema"], rede.contatoDoSistema)
+    pp.add_int(element_dic_inv["httpPort"], rede.httpPort)
+    pp.add_int(element_dic_inv["useHttps"], rede.useHttps)
+    pp.add_int(element_dic_inv["httpsPort"], rede.httpsPort)
+    pp.add_int(element_dic_inv["httpTempoDeAtualizacao"], rede.httpTempoDeAtualizacao)
+    pp.add_str(element_dic_inv["paginaPadraoHttp"], rede.paginaPadraoHttp)
 
 def main_update():
-    for element in element_list:
-        pp.add_cnt_64bit(element, value_list[element_list[element]])
+    logging.info("Main update")
+    session = Session()
+    logging.info("Aberta sessao com o BD")
+    # Query parameters from database, one object at a time
+    p = session.query(Parameters).first()
+    # Post parameters related to that object
+    addParametersToOIDs(p, pp)
 
+    m = session.query(Modulo).first()
+    addModultoToOIDs(m, pp)
+
+    u = session.query(Usuario).first()
+    addUsuarioToOIDs(u, pp)
+
+    r = session.query(RedeSeguranca).first()
+    addRedeToOIDs(r, pp)
+    session.close()
+
+logging.info("Registra OID")
 pp = snmp.PassPersist('.1.3.6.1.4.1.39178.100.1.1.1.2')
 pp.start(main_update, 10)
---
+++ db_engine.py
@@ -0,0 +1,150 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+# engine = create_engine('sqlite:////home/prjs/cm_comandos_lineares/equalizer-agent/equalizerdb', echo=False)
+engine = create_engine('sqlite:///home/root/equalizer-agent/equalizerdb', echo=False)
+
+Base = declarative_base()
+
+# Database classes, reflect the schema in wiki
+class Usuario(Base):
+    __tablename__ = "Usuario"
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    sobreNome = Column(String)
+    telefone = Column(String)
+    email = Column(String)
+    senha = Column(String)
+    acesso = Column(String)
+
+
+class Modulo(Base):
+    __tablename__ = 'Modulo'
+
+    id = Column(Integer, primary_key=True)
+    descricao = Column(String)
+    tensao_nominal = Column(Float)
+    capacidade_nominal = Column(Integer)
+    n_strings = Column(Integer)
+    n_baterias_por_strings = Column(Integer)
+    contato = Column(String)
+    localizacao = Column(String)
+    fabricante = Column(String)
+    tipo = Column(String)
+    data_instalacao = Column(String)
+
+    conf_alarme_id = Column(Integer, ForeignKey('AlarmeConfig.id'))
+    alarme_config = relationship("AlarmeConfig")
+
+class AlarmeConfig(Base):
+    __tablename__ = "AlarmeConfig"
+
+    id = Column(Integer, primary_key=True)
+    tipo_modulo = Column(String)
+    nivel_alert_tensao_max = Column(Float)
+    nivel_alert_tensao_min = Column(Float)
+    nivel_alert_temp_max = Column(Float)
+    nivel_alert_temp_min = Column(Float)
+    nivel_alert_impedancia_max = Column(Float)
+    nivel_alert_impedancia_min = Column(Float)
+    nivel_max_tensao_ativo = Column(Integer)
+    nivel_max_tensao_val = Column(Float)
+    alarme_nivel_tensao_max = Column(Float)
+    alarme_nivel_tensao_min = Column(Float)
+    alarme_nivel_temp_max = Column(Float)
+    alarme_nivel_temp_min = Column(Float)
+    alarme_nivel_imped_max = Column(Float)
+    alarme_nivel_imped_min = Column(Float)
+
+class RedeSeguranca(Base):
+    __tablename__ = "RedeSeguranca"
+
+    id = Column(Integer, primary_key=True)
+    mac = Column(String)
+    velocidadePlacaRede = Column(String)
+    localAddress = Column(String)
+    gateway = Column(String)
+    mascara = Column(String)
+    servidorDNS = Column(String)
+    nomeDoSistema = Column(String)
+    localDoSistema = Column(String)
+    contatoDoSistema = Column(String)
+    httpPort = Column(Integer)
+    useHttps = Column(Integer)
+    httpsPort = Column(Integer)
+    httpTempoDeAtualizacao = Column(Integer)
+    paginaPadraoHttp = Column(String)
+
+class DataLog(Base):
+    __tablename__ = "DataLog"
+
+    id = Column(Integer, primary_key=True)
+    dataHora = Column(String)
+    string = Column(String)
+    bateria = Column()
+    temperatura = Column(Float)
+    impedancia = Column(Float)
+    tensao = Column(Float)
+    equalizacao = Column(Float)
+
+class TimeServer(Base):
+    __tablename__ = "TimeServer"
+
+    id = Column(Integer, primary_key=True)
+    timeServerAddress1 = Column(String)
+    timeServerAddress1_complemento = Column(String)
+    timeServerAddress2 = Column(String)
+    timeServerAddress2_complemento = Column(String)
+    timeServerAddress3 = Column(String)
+    timeServerAddress3_complemento = Column(String)
+    connectionRetries = Column(Integer)
+    timeZone = Column(String)
+    automAdjustTimeDaylightSavingChanges = Column(Integer)
+
+class EmailServer(Base):
+    __tablename__ = "EmailServer"
+
+    id = Column(Integer, primary_key=True)
+    server = Column(String)
+    portaSMTP = Column(Integer)
+    usarCriptografiaTLS = Column(Integer)
+    email = Column(String)
+    assunto = Column(String)
+    usarAutenticacao = Column(Integer)
+    login = Column(String)
+    senha = Column(String)
+
+class AlarmLog(Base):
+    __tablename__ = "AlarmLog"
+
+    id = Column(Integer, primary_key=True)
+    dataHora = Column(String)
+    descricao = Column(String)
+    emailEnviado = Column(Integer)
+
+class Parameters(Base):
+    __tablename__ = "Parameters"
+
+    id = Column(Integer, primary_key=True)
+    avg_last = Column(String)
+    duty_min = Column(String)
+    duty_max = Column(String)
+    cte_index = Column(String)
+    delay = Column(String)
+    num_cycles_var_read = Column(String)
+    bus_voltage = Column(String)
+    save_log_time = Column(String)
+    disk_capacity = Column(String)
+    param1 = Column(String)
+    param2 = Column(String)
+    param3 = Column(String)
+    param4 = Column(String)
+    param5 = Column(String)
+    param6 = Column(String)
+    param7 = Column(String)
+    param8 = Column(String)
+    param9 = Column(String)
+    param10 = Column(String)
