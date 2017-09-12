0b1111ed29b4c64c4f9381dbb986d6b3f3e3a60c
Marcelo Mergulhao <marcelo.mergulhao@phiinnovations.com>
2017-09-12 14:25:08 -0300

    Adicionando tratamentos de links para manter serviços ligados ou desligados de acordo com o agent_config

--- config_agent.py
+++ config_agent.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
 import logging
+from os import unlink, symlink
+from os.path import islink
 from db_engine import engine, SNMPConfig
 from sqlalchemy.orm import sessionmaker
 from subprocess import call
@@ -25,6 +27,18 @@ def start_trap_service():
 def start_snmpd_service():
     logging.info("Starting snmpd service")
     call(["/etc/init.d/snmpd", "start"])
+
+def link_script(dst, link_name):
+    symlink("/etc/init.d/{}".format(dst), "/etc/rc2.d/{}".format(link_name))
+    symlink("/etc/init.d/{}".format(dst), "/etc/rc3.d/{}".format(link_name))
+    symlink("/etc/init.d/{}".format(dst), "/etc/rc4.d/{}".format(link_name))
+    symlink("/etc/init.d/{}".format(dst), "/etc/rc5.d/{}".format(link_name))
+
+def unlink_script(link_name):
+    unlink("/etc/rc2.d/{}".format(link_name))
+    unlink("/etc/rc3.d/{}".format(link_name))
+    unlink("/etc/rc4.d/{}".format(link_name))
+    unlink("/etc/rc5.d/{}".format(link_name))
 
 def main_cli():
     """Enable SNMP agent parametrization"""
@@ -52,8 +66,23 @@ def main_cli():
             session.commit()
         if config["trapSender"] == "on":
             start_trap_service()
+            if not islink("/etc/rc2.d/S95equalizer-traps"):
+                # Create symlinks
+                link_script("equalizer-traps", "S95equalizer-traps")
+        else:
+            #Make sure it doesn't start again on the next startup
+            if islink("/etc/rc2.d/S95equalizer-traps"):
+                # Remove symlinks
+                unlink_script("S95equalizer-traps")
         if config["snmpAgent"] == "on":
             start_snmpd_service()
+            if not islink("/etc/rc2.d/S90snmpd"):
+                # Create symlinks
+                link_script("snmpd", "S90snmpd")
+        else:
+            if islink("/etc/rc2.d/S90snmpd"):
+                # Remove symlinks
+                unlink_script("S90snmpd")
     else:
         logging.info("Não há arquivo de configuração disponível")
 
